542f0c251dc7d4be22cf0722ac87b8e06eb88d64
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2021-12-13 13:40:20 -0800

    example file, this is how you would test if TS is working properly

---
+++ modules/apps/frontend-js/frontend-js-react-web/src/main/resources/META-INF/resources/js/test.ts
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+// Your editor should now give you type help for these below
+
+Liferay.Address.getCountries();
+Liferay.Address.getRegions();
+Liferay.Util.fetch();
+Liferay.Util.formatStorage();
+Liferay.Util.formatXML();
+
+// Note that this definition doesn't exist, so TS complains about it.
+
+Liferay.SomeNamespace.foo();
