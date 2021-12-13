1a2ff60aba2f2e8993056c141a6b98ba33915b67
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2021-12-13 13:20:18 -0800

    Update global Liferay types to be mroe accurate

--- modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/liferay.d.ts
+++ modules/apps/frontend-js/frontend-js-web/src/main/resources/META-INF/resources/liferay/liferay.d.ts
@@ -1,12 +1,38 @@
-export function getCountries(callback: () => void): Promise<any>;
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
 
-export function getRegions(
-	callback: () => void,
-	selectKey: string
-): Promise<any>;
+declare module Liferay {
+	namespace Address {
 
-export function fetch(resource: string | Request, init?: Object): Promise<any>;
+		// This is declaring a type for `Liferay.Address.getCountries()`.
 
-export function formatStorage(size: number, options?: Object): string;
+		export function getCountries(callback: () => void): Promise<any>;
 
-export function formatXML(content: string, options?: Object): string;
+		export function getRegions(
+			callback: () => void,
+			selectKey: string,
+		): Promise<any>;
+	}
+
+	namespace Util {
+		export function fetch(
+			resource: string | Request,
+			init?: Object,
+		): Promise<any>;
+
+		export function formatStorage(size: number, options?: Object): string;
+
+		export function formatXML(content: string, options?: Object): string;
+	}
+}
